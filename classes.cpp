c948d53d356c2bba8b27d65f723cd59d2bc492c2
Amirjon Nazarov <a3465172@gmail.com>
2025-01-08 13:12:13 +0500

    D

--- classes.cpp
+++ classes.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 class Human {
     public:
-    
+
         int age;
+        int weight;
         string name;
 
 };
@@ -15,6 +16,22 @@ class Human {
 int main() {
 
     Human firstHuman;
+
+    firstHuman.age = 18;
+    firstHuman.name = "amir";
+    firstHuman.weight = 75;
+
+    cout << firstHuman.name << " ";
+    cout << firstHuman.age << endl;
+    cout << firstHuman.weight << " kg"<< endl;
+
+    Human secodnhuman;
+    secodnhuman.age = 24;
+    secodnhuman.name = "nazar";
+    secodnhuman.weight = 50;
+    cout << secodnhuman.name << " ";
+    cout << secodnhuman.age << endl;
+    cout << secodnhuman.weight << " kg"<< endl;
 
     return 0;
 }
