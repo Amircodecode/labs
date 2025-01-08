19244e67eba7960200a907b210822e7299de5fec
Amirjon Nazarov <a3465172@gmail.com>
2025-01-08 12:33:26 +0500

    D

--- finalProjectMyself.cpp
+++ finalProjectMyself.cpp
@@ -22,10 +22,22 @@ int main() {
 
     for (int i = 0; i < numberStudent - 1; i++) {
         for (int j = 0; j < numberStudent - i - 1; j++) {
-            if (scores[j] > scores[j + 1]) {
-                
+            if (scores[j] < scores[j + 1]) {
+                int tempScore = scores[j];
+                scores[j] = scores[j + 1];
+                scores[j + 1] = tempScore;
+
+                string tempName = names[j];
+                names[j] = names[j + 1];
+                names[j + 1] = tempName;
             }
         }
     }
+
+    int passStudents[numberStudent];
+    string passNames[numberStudent];
+    int cout = 0;
+
+    
     return 0;
 }
