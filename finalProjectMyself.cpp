6a1c0381b1dad749c4e8c7e8fbf30491991d0194
Amirjon Nazarov <a3465172@gmail.com>
2025-01-08 12:24:54 +0500

    D

--- finalProjectMyself.cpp
+++ finalProjectMyself.cpp
@@ -12,6 +12,20 @@ int main() {
     int *scores = new int[numberStudent];
     string *names = new string[numberStudent];
 
-    
+    for (int i = 0; i < numberStudent; i++) {
+        cout << "enter the " << i + 1 << " student name ";
+        cin >> names[i];
+
+        cout << "enter the " << i + 1 << " score ";
+        cin >> scores[i];
+    }
+
+    for (int i = 0; i < numberStudent - 1; i++) {
+        for (int j = 0; j < numberStudent - i - 1; j++) {
+            if (scores[j] > scores[j + 1]) {
+                
+            }
+        }
+    }
     return 0;
 }
