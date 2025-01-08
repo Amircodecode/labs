a4f7441396f721718624798d8105ecf87f777c99
Amirjon Nazarov <a3465172@gmail.com>
2025-01-08 16:10:39 +0500

    D

--- classes2.cpp
+++ classes2.cpp
@@ -34,30 +34,57 @@ using namespace std;
 //     return 0;
 // }
 
+// class Point {
+//     public:
+//         int x;
+//         int y;
+
+//         void Print() {
+//             cout << "y = " << y << " x = " << x << " z = " << z << endl;
+
+//             PrintY();
+//         }
+    
+//     private:
+//         int z;
+
+//         void PrintY() {
+//             cout << " PrintY " << "z = " << z << endl;
+//         }
+// };
+
+// int main() {
+//     Point a;
+//     a.Print();
+
+
+//     return 0;
+// }
+
 class Point {
-    public:
+    private:
         int x;
         int y;
 
-        void Print() {
-            cout << "y = " << y << " x = " << x << " z = " << z << endl;
-
-            PrintY();
+    public:
+        int GetX() {
+            return x;
         }
-    
-    private:
-        int z;
 
-        void PrintY() {
-            cout << " PrintY " << "z = " << z << endl;
+        void SetX(int valueX) {
+            x = valueX;
+        }
+
+        void Print () {
+            cout << "x = " << x << " y = " << y << endl;
         }
 };
 
 int main() {
+
     Point a;
     a.Print();
-
+    
 
     return 0;
 }
-
