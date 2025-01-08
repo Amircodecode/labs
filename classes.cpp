367b402ad9954bad143f37fd160fb09688ecfaf1
Amirjon Nazarov <a3465172@gmail.com>
2025-01-08 13:16:47 +0500

    D

--- classes.cpp
+++ classes.cpp
@@ -13,25 +13,39 @@ class Human {
 };
 
 
+class Point {
+    public:
+        int x;
+        int y; 
+        int z;
+};
+
 int main() {
 
     Human firstHuman;
 
-    firstHuman.age = 18;
-    firstHuman.name = "amir";
-    firstHuman.weight = 75;
+    // firstHuman.age = 18;
+    // firstHuman.name = "amir";
+    // firstHuman.weight = 75;
 
-    cout << firstHuman.name << " ";
-    cout << firstHuman.age << endl;
-    cout << firstHuman.weight << " kg"<< endl;
+    // cout << firstHuman.name << " ";
+    // cout << firstHuman.age << endl;
+    // cout << firstHuman.weight << " kg"<< endl;
 
-    Human secodnhuman;
-    secodnhuman.age = 24;
-    secodnhuman.name = "nazar";
-    secodnhuman.weight = 50;
-    cout << secodnhuman.name << " ";
-    cout << secodnhuman.age << endl;
-    cout << secodnhuman.weight << " kg"<< endl;
+    // Human secodnhuman;
+    // secodnhuman.age = 24;
+    // secodnhuman.name = "nazar";
+    // secodnhuman.weight = 50;
+    // cout << secodnhuman.name << " ";
+    // cout << secodnhuman.age << endl;
+    // cout << secodnhuman.weight << " kg"<< endl;
+
+    // Point a;
+    // a.x = 1;
+    // a.y = 2;
+    // a.z = 8;
+
+    // cout << "(" << a.x << ", " << a.y << ", " << a.z << ")" << endl;
 
     return 0;
 }
