baee3e4ac531c3972e5c69a52d843fdecaf5a0b0
Amirjon Nazarov <a3465172@gmail.com>
2025-01-08 15:48:43 +0500

    D

--- classes2.cpp
+++ classes2.cpp
@@ -2,34 +2,56 @@
 #include <string>
 using namespace std;
 
-class Human {
+// class Human {
 
-    public:
-        int age;
-        int weight;
-        string name;
+//     public:
+//         int age;
+//         int weight;
+//         string name;
     
+//         void Print() {
+
+//             cout << "name: " << name << 
+//                     " weight " << weight <<     
+//                     " age " << age << endl;
+
+//         }
+
+// };
+
+
+// int main() {
+//     Human firstHuman;
+
+//     firstHuman.age = 18;
+//     firstHuman.name = "Amir";
+//     firstHuman.weight = 75;
+
+//     // cout << firstHuman.age << firstHuman.name << firstHuman.weight << endl;
+
+//     firstHuman.Print();
+
+//     return 0;
+// }
+
+class Point {
+    public:
+        int x;
+        int y;
+
         void Print() {
-
-            cout << "name: " << name << 
-                    " weight " << weight <<     
-                    " age " << age << endl;
-
+            cout << "y = " << y << " x = " << x << " z = " << z << endl;
         }
-
+    
+    private:
+        int z;
 };
 
-
 int main() {
-    Human firstHuman;
+    Point a;
+    a.Print();
 
-    firstHuman.age = 18;
-    firstHuman.name = "Amir";
-    firstHuman.weight = 75;
 
-    // cout << firstHuman.age << firstHuman.name << firstHuman.weight << endl;
-
-    firstHuman.Print();
-    
     return 0;
 }
+
