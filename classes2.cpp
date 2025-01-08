872ab7ffbe7485484cf6c94d6a4ee1b36a58077f
Amirjon Nazarov <a3465172@gmail.com>
2025-01-08 15:22:37 +0500

    D

---
+++ classes2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Human {
+    
+    public:
+        int age;
+        int weight;
+        string name;
+};
+
+
+int main() {
+    return 0;
+}
