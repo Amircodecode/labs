a8628b8c0185de4b285a5a795938eafa7bab38a8
Amirjon Nazarov <a3465172@gmail.com>
2025-01-08 12:19:40 +0500

    D

---
+++ finalProjectMyself.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+    int numberStudent;
+    cout << "enter the number of studets: ";
+    cin >> numberStudent;
+
+    int *scores = new int[numberStudent];
+    string *names = new string[numberStudent];
+
+    
+    return 0;
+}
