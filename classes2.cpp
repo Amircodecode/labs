2375d159161c6a583a0cb90c576bce47b6af48dc
Amirjon Nazarov <a3465172@gmail.com>
2025-01-09 12:11:09 +0500

    D

--- classes2.cpp
+++ classes2.cpp
@@ -422,3 +422,34 @@ class Point {
 // }
 
 
+class Point {
+    private: 
+        int x;
+        int y;
+
+    public:
+        Point() {
+            x = 0;
+            y = 0;
+        }
+
+        Point(int x, int y) {
+            this -> x = x;
+            this -> y = y;
+        }
+        void setX(int x) {
+            this -> x = x;
+        }
+        void setY(int y) {
+            this -> y = y;
+        }
+        int getX() {
+            return x;
+        }
+        int getY() {
+            return y;
+        }
+        void Print() {
+            cout << "x = " << x << " y = " << y << endl;
+        }
+};
